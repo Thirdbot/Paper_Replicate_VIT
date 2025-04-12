ac76e635d931dfbd0bfa7f74219857cb4fdd12ac
Third <astroll656@gmail.com>
2025-04-12 21:20:23 +0700

    getting data from dataloader

--- main.py
+++ main.py
@@ -8,9 +8,38 @@ from torch import nn
 from torchvision import transforms
 from torchinfo import summary
 from going_modular import data_setup,engine
-
+from helper_functions import download_data
 
 #set up device for gpu
-
 device = 'cuda' if torch.cuda.is_available() else "cpu"
 print(f"Using:{device}")
+
+# Download pizza, steak, sushi images from GitHub
+image_path = download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
+                           destination="pizza_steak_sushi")
+
+##args
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+batch_size = 32
+NumberOfWorker =2
+IMG_SIZE = 244 #from vit paper
+
+#resize data incoming
+manual_transforms = transforms.Compose([
+    transforms.Resize((IMG_SIZE,IMG_SIZE)),
+    transforms.ToTensor()
+])
+
+print(f"Path Train:{train_dir}\nPath Test:{test_dir}")
+print(f"Print Out Manually Created Transforms:{manual_transforms}")
+
+train_dataloader,test_dataloader,classes_name = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=manual_transforms,
+    batch_size=batch_size,
+    num_workers=NumberOfWorker
+)
+
+print(f"Training data size: {len(train_dataloader)}\nTesting data size: {len(test_dataloader)}\nclasses Name: {classes_name}")
