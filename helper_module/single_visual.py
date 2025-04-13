714867e50baaa68da53d374b79f006eedef5a2b5
Third <astroll656@gmail.com>
2025-04-13 18:32:31 +0700

    change

--- helper_module/single_visual.py
+++ helper_module/single_visual.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def single_image_visual(image,classes,label):
-    plt.imshow(image.permute(1,2,0))
+    plt.imshow(image)
     plt.title(classes[label])
     plt.axis(False)
     plt.show()
