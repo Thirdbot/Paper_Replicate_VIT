703b32e6f325c3e9067175f7c50a12f887f1bb38
Third <astroll656@gmail.com>
2025-04-16 10:51:05 +0700

    change graph

--- helper_functions.py
+++ helper_functions.py
@@ -137,11 +137,11 @@ def plot_loss_curves(results):
              "test_loss": [...],
              "test_acc": [...]}
     """
-    loss = results["train_loss"]
-    test_loss = results["test_loss"]
+    vit_loss = results["vit_train_loss"]
+    weight_loss = results["weight_train_loss"]
 
-    accuracy = results["train_acc"]
-    test_accuracy = results["test_acc"]
+    vit_accuracy = results["vit_train_acc"]
+    weight_accuracy = results["weight_train_acc"]
 
     epochs = range(len(results["train_loss"]))
 
@@ -149,16 +149,16 @@ def plot_loss_curves(results):
 
     # Plot loss
     plt.subplot(1, 2, 1)
-    plt.plot(epochs, loss, label="train_loss")
-    plt.plot(epochs, test_loss, label="test_loss")
+    plt.plot(epochs, vit_loss, label="vit_train_loss")
+    plt.plot(epochs, weight_loss, label="weight_train_loss")
     plt.title("Loss")
     plt.xlabel("Epochs")
     plt.legend()
 
     # Plot accuracy
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, accuracy, label="train_accuracy")
-    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.plot(epochs, vit_accuracy, label="vit_train_accuracy")
+    plt.plot(epochs, weight_accuracy, label="weight_train_accuracy")
     plt.title("Accuracy")
     plt.xlabel("Epochs")
     plt.legend()
